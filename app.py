512bcf6c7b5e19987ce091c85cadd30dfba83cca
carolxu2006 <carolxu2006@gmail.com>
2025-08-16 12:45:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,17 @@ def page3():
         conn = sqlite3.connect('choices.db')
         conn.row_factory = sqlite3.Row #makes it like a dictionary
         cursor = conn.cursor()
-        cursor.execute("SELECT genre1,genre2,genre3 FROM users WHERE id = ?", (id,))
-        user_genres = cursor.fetchone()
+        cursor.execute("INSERT INTO choices (user_id, title, author) VALUES(?,?,?)",(session["user_id"], request.form.get("choice"),request.form.get("choice2")))
+        conn.commit()
+        conn.close()
+        return redirect("/page3")
+    else:
+        id = session["user_id"]
+        conn1 = sqlite3.connect('users.db')
+        conn1.row_factory = sqlite3.Row #makes it like a dictionary
+        cursor1 = conn1.cursor()
+        cursor1.execute("SELECT genre1,genre2,genre3 FROM users WHERE id = ?", (id,))
+        user_genres = cursor1.fetchone()
 
         conn2 = sqlite3.connect('books.db')
         conn2.row_factory = sqlite3.Row #makes it like a dictionary
@@ -120,6 +129,7 @@ def page3():
         book['genres'] = ast.literal_eval(book['genres'])
         book['publication_info'] = ast.literal_eval(book['publication_info']) 
         return render_template("page3.html", book = book)
+
 
 @app.route("/page4", methods=["GET", "POST"])
 @login_required
