b37a96885434fb96d948cac39216b85754760707
carolxu2006 <carolxu2006@gmail.com>
2025-08-16 13:31:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def after_request(r):
     return r
 
 def login_required(f):
+    """Creates decorator to reroute to login page if not logged in yet"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
@@ -46,6 +47,7 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
+    #pulls user info from database
     if request.method == "POST":
         conn = sqlite3.connect('users.db')
         conn.row_factory = sqlite3.Row #makes it like a dictionary
@@ -53,9 +55,11 @@ def login():
         cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
         users = [dict(row) for row in cursor.fetchall()]
         conn.close()
-
+        
+        #checks if user is in database and if the password is correct
         if len(users) != 1 or not check_password_hash(users[0]["password"], request.form.get("password")):
             return("invalid username and/or password")
+        #logs user in and tracks their session info
         session["user_id"] = users[0]["id"]
         return redirect("/")
     else:
@@ -69,6 +73,7 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     session.clear()
+    #checks if passwords entered match
     if request.method == "POST":
         if request.form.get("password") != request.form.get("confirmpassword"):
             return "passwords don't match"
@@ -77,6 +82,7 @@ def register():
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
         users = [dict(row) for row in cursor.fetchall()]
+        #checks if username is unique and enters user info into the database with a hash of their password
         try:
             username = request.form.get("username")
             passwordhash = generate_password_hash(request.form.get("password"))
@@ -94,6 +100,7 @@ def register():
 @app.route("/page3", methods=["GET", "POST"])
 @login_required
 def page3():
+    #when user swipes right, pulls info from html form and save the info from the book they liked and shows a new book on the page
     if request.method == "POST":
         conn = sqlite3.connect('choices.db')
         conn.row_factory = sqlite3.Row #makes it like a dictionary
@@ -102,6 +109,8 @@ def page3():
         conn.commit()
         conn.close()
         return redirect("/page3")
+        
+    #displays book based on the genres that they chose on the preferences page
     else:
         id = session["user_id"]
         conn1 = sqlite3.connect('users.db')
@@ -113,22 +122,23 @@ def page3():
         conn2 = sqlite3.connect('books.db')
         conn2.row_factory = sqlite3.Row #makes it like a dictionary
         cursor2 = conn2.cursor()
-        
+        #checks if user has entered their preferences
         if user_genres == None:
             genres = [user_genres['genre1'], user_genres['genre2'], user_genres['genre3']]
             cursor2.execute("SELECT * FROM books WHERE genres LIKE ? OR genres LIKE ? OR genres LIKE ?", ('%' + genres[0] + '%', '%' + genres[1] + '%', '%' + genres[2] + '%'))
         else:
             cursor2.execute("SELECT * FROM books")
+        #format and sends info from database to html to be used
         books = [dict(row) for row in cursor2.fetchall()]
         book = random.choice(books)
         book['genres'] = ast.literal_eval(book['genres'])
         book['publication_info'] = ast.literal_eval(book['publication_info']) 
         return render_template("page3.html", book = book)
 
-
 @app.route("/page4", methods=["GET", "POST"])
 @login_required
 def page4():
+    #formats and sends info from database to html to be used
     if request.method == "POST":
         conn = sqlite3.connect('books.db')
         conn.row_factory = sqlite3.Row #makes it like a dictionary
@@ -144,6 +154,7 @@ def page4():
 @app.route("/preferences", methods=["GET", "POST"])
 @login_required
 def preferences():
+    #gets preferences from user input from the html form and stores it in the appropriate database
     if request.method == "POST":
         uid = session["user_id"]
 
@@ -169,7 +180,7 @@ def preferences():
         conn.close()
 
         return redirect("/")
-    
+    #display the proper information on the html page
     else:
         uid = session.get("user_id")
         conn = sqlite3.connect('users.db')
